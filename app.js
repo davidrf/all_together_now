f8b9037c826f819c5a732884267c4228cf4aa628
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2016-01-08 19:46:36 -0500

    Finish exercise

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 var express = require('express'),
     app = express(),
-    engines = require('consolidate');
+    engines = require('consolidate'),
+    MongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
 
 app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
-app.get('/', function(req, res) {
-  res.render('hello', { 'name': 'Templates'});
-});
+MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
+  assert.equal(null, err);
+  console.log("Successfully connected to MongoDB.");
 
-app.use(function(req, res) {
-  res.sendStatus(404);
-});
+  app.get('/', function(req, res) {
+    db.collection('movies').find({}).toArray(function(err, docs) {
+      res.render('movies', { 'movies': docs } );
+    });
+  });
 
-var server = app.listen(3000, function() {
-  var port = server.address().port;
-  console.log('Express sever listening on port %s', port);
-})
+  app.use(function(req, res) {
+    res.sendStatus(404);
+  });
+
+  var server = app.listen(3000, function() {
+    var port = server.address().port;
+    console.log('Express sever listening on port %s', port);
+  })
+});
